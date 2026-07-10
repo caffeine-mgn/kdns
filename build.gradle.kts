475cdd74461ce393aefc583f7eb302fd8a1b4908
subochev <caffeine.mgn@gmail.com>
2026-07-11 02:32:55 +0300

    Add Dokka plugin, configure Maven Central release workflow, and update publishing settings.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlinx.serialization) apply false
-    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
 }
 allprojects {
 //    if (version == "unspecified") {
 //        version = "1.0.0-SNAPSHOT"
 //    }
-    version = "1.0.0-SNAPSHOT"
-//    version = "0.0.1"
+    version = "0.0.1"
+//    version = "1.0.0-SNAPSHOT"
+//    version = "0.0.1-SNAPSHOT"
     group = "pw.binom.dns"
 
     repositories {
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -2,8 +2,8 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
-    id("maven-publish")
-    id("signing")
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.vanniktech.maven.publish)
 }
 
 kotlin {
@@ -23,14 +23,13 @@ kotlin {
     mingwX64()
     macosX64()
     macosArm64()
-    macosX64()
-    macosArm64()
     iosX64()
     iosArm64()
     iosSimulatorArm64()
     tvosX64()
     tvosArm64()
     tvosSimulatorArm64()
+    @OptIn(org.jetbrains.kotlin.gradle.ExperimentalWasmDsl::class)
     wasmJs {
         browser()
         d8()
@@ -55,69 +54,38 @@ kotlin {
     }
 }
 
-signing {
-    useInMemoryPgpKeys(
-        providers.gradleProperty("binom.gpg.key_id").orNull ?: System.getenv("GPG_KEY_ID"),
-        providers.gradleProperty("binom.gpg.private_key").orNull ?: System.getenv("GPG_PASSWORD"),
-        providers.gradleProperty("binom.gpg.password").orNull ?: System.getenv("GPG_PRIVATE_KEY")
+mavenPublishing {
+    publishToMavenCentral(automaticRelease = true)
+    signAllPublications()
+    coordinates(
+        groupId = "pw.binom.dns",
+        artifactId = "core",
+        version = project.version.toString()
     )
-    sign(publishing.publications)
-}
 
+    pom {
+        name.set("kdns")
+        description.set("Kotlin DNS library for Multiplatform")
+        url.set("https://github.com/caffeine-mgn/kdns")
 
-
-publishing {
-    publications.withType<MavenPublication> {
-//        groupId = "pw.binom.dns"
-//        artifactId = "protocol"
-//        version = project.version.toString()
-
-        pom {
-            name.set("kdns")
-            description.set("Kotlin DNS library for Multiplatform")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("caffeine-mgn")
+                name.set("Anton")
+                email.set("caffeine.mgn@gmail.com")
+            }
+        }
+        scm {
+            connection.set("scm:git:git://github.com/caffeine-mgn/kdns.git")
+            developerConnection.set("scm:git:ssh://github.com/caffeine-mgn/kdns.git")
             url.set("https://github.com/caffeine-mgn/kdns")
-
-            licenses {
-                license {
-                    name.set("The Apache License, Version 2.0")
-                    url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                }
-            }
-            developers {
-                developer {
-                    id.set("caffeine-mgn")
-                    name.set("Anton")
-                    email.set("caffeine.mgn@gmail.com")
-                }
-            }
-            scm {
-                connection.set("scm:git:git://github.com/caffeine-mgn/kdns.git")
-                developerConnection.set("scm:git:ssh://github.com/caffeine-mgn/kdns.git")
-                url.set("https://github.com/caffeine-mgn/kdns")
-            }
         }
-    }
-
-    repositories {
-        mavenLocal()
-        if (project.version.toString().endsWith("-SNAPSHOT")) {
-            maven {
-                name = "sonatype"
-                url = uri("https://central.sonatype.com/repository/maven-snapshots/")
-
-                credentials {
-                    username = providers.gradleProperty("mavenCentralPortalUsername").orNull
-                        ?: System.getenv("SONATYPE_USERNAME")
-                    password = providers.gradleProperty("mavenCentralPortalPassword").orNull
-                        ?: System.getenv("SONATYPE_PASSWORD")
-                }
-            }
-        }
-    }
-}
-
-afterEvaluate {
-    publishing.publications.all {
-        println("Publication: $name")
     }
 }
