d0216c2c393499d75a74b698fa415b5ce38bce77
subochev <caffeine.mgn@gmail.com>
2026-07-11 04:51:49 +0300

    `chore: set default version to 1.0.0-SNAPSHOT`

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,9 @@ plugins {
     alias(libs.plugins.kotlinx.serialization) apply false
 }
 allprojects {
-//    if (version == "unspecified") {
-//        version = "1.0.0-SNAPSHOT"
-//    }
-    version = "0.0.1"
-//    version = "1.0.0-SNAPSHOT"
-//    version = "0.0.1-SNAPSHOT"
+    if (version == "unspecified") {
+        version = "1.0.0-SNAPSHOT"
+    }
     group = "pw.binom.dns"
 
     repositories {
